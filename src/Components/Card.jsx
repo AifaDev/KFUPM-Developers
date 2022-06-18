05ac56ed0eb6efbf575c965660a0fa94f7ad3655
Ibrahim Beladi <ibrahimbeladi@outlook.com>
2022-06-18 15:03:42 +0300

    use grid in competition tab

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,14 +4,16 @@ export default function Card({ name, theme, startingDate, Deadline, status }) {
   return (
     <Template>
       <h4>{name}</h4>
-      <h5>Theme:</h5>
-      <p>{theme}</p>
-      <h5>Starting Date:</h5>
-      <p>{startingDate}</p>
-      <h5>Deadline:</h5>
-      <p>{Deadline}</p>
-      <h5>Status:</h5>
-      <p>{status}</p>
+      <div class="content">
+        <h5>Theme:</h5>
+        <p>{theme}</p>
+        <h5>Starting Date:</h5>
+        <p>{startingDate}</p>
+        <h5>Deadline:</h5>
+        <p>{Deadline}</p>
+        <h5>Status:</h5>
+        <p>{status}</p>
+      </div>
     </Template>
   );
 }
@@ -24,4 +26,15 @@ const Template = styled.div`
   padding: 1.5rem;
   display: grid;
   gap: 5px;
+
+  .content {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+  }
+
+  @media (max-width: 450px) { 
+    .content {
+      display: initial;
+    }
+  }
 `;
